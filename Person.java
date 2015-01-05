073aa325f511d226b55a7559d70cbad11b1281c4
lyg <liuyuguoyxl@163.com>
2015-01-05 23:08:10 +0800

    update Person.java

--- Person.java
+++ Person.java
@@ -22,4 +22,16 @@ public class Person{
 	public String getName(){
 		return this.name;
 	}
+	
+	public void setAge(Integer age){
+		this.age = age;
+	}
+	
+	public Integer getAge(){
+		return this.age;
+	}
+	
+	public void addNewMethod(){
+		System.out.println('this is a new line!')
+	}
 }
